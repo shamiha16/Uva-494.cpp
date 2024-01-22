dbd55c9de30a2f907667818b6bfeca7a291d690b
shamiha16 <155057582+shamiha16@users.noreply.github.com>
2024-01-22 17:55:18 +0600

    Update Uva 494.cpp

--- Uva 494.cpp
+++ Uva 494.cpp
@@ -1,21 +1,33 @@
 #include<stdio.h>
-#include<ctype.h>
-int main(){
-    char ch;
-    int c=0,in_word=0;
-    while((ch=fgetchar())!=EOF){
-          if(isalpha(ch))
-             {in_word=1;}
-             else if (isspace(ch)&& in_word)
-               {
+#include<string.h>
+int main()
+{
+    char text[10000];
+    int i,word,strt;
+    while(gets(text))
+    {
+        word=0;
+        strt=0;
+        for(i=0;text[i]!='\0';i++)
+        {
+            if((text[i]>='a' && text[i]<='z')||(text[i]>='A' && text[i]<='Z') )
+                strt=1;
+            else
+            {
+                if(strt)
+                {
+                    word++;
+                    strt=0;
+                }
+            }
+        }
+        if(text[i]=='\0'&&strt==1)
+            word++;
+        printf("%d\n", word);
+    }
+    return 0;
 
-               c++;
-                in_word=0;
-             }}
-             if (in_word)
-             {
-                c++;}
-            printf("%d\n",c);
-            return 0;}
+}
+
 
 
